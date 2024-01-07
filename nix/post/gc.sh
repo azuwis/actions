811d28e9ccc322451e8f79a5245aba5c8a282549
Zhong Jianxin <azuwis@gmail.com>
2024-01-07 20:25:15 +0800

    nix/post: Always run nix gc, auto mark cache need update based on gcroots entries

--- nix/post/gc.sh
+++ nix/post/gc.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# create gcroots for flake inputs to prevent gc, note the flake itself is excluded
 if [ -f flake.nix ]
 then
-  # create gcroots for flake inputs to prevent gc
-  flake_inputs=$(nix flake archive --json --no-write-lock-file)
-  while [[ $flake_inputs =~ /nix/store/[^\"]+ ]]
+  nix flake archive --json --no-write-lock-file | jq -r '.inputs | .. | .path? // empty' | while read -r store_path
   do
-    store_path="${BASH_REMATCH[0]}"
     nix build --out-link "/tmp/${store_path##*/}" "$store_path"
-    flake_inputs="${flake_inputs/${store_path}/}"
   done
 fi
 
 nix-store --gc
 nix-store --optimise
-nix-store --gc --print-roots
+nix-store --gc --print-roots | grep -v -F -e '/proc/' -e '{lsof}'
+
+mv -v /nix/var/gcroots /nix/var/gcroots-old || true
+nix-store --gc --print-roots | grep -v -F -e '/proc/' -e '{lsof}' | awk '{print $3}' | sort > /nix/var/gcroots
+
+if [ -f /nix/var/gcroots-old ]
+then
+  if diff -u /nix/var/gcroots-old /nix/var/gcroots
+  then
+    echo "Gcroots unchanged"
+  else
+    echo "Gcroots changed, mark cache need update"
+    echo "CACHE_NEED_UPDATE=yes" >> "$GITHUB_ENV"
+  fi
+else
+  echo "Gcroots does not exist, mark cache need update"
+  echo "CACHE_NEED_UPDATE=yes" >> "$GITHUB_ENV"
+fi
