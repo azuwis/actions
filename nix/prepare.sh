c4b1ac3f09ccf7de06213d6addf041758822d6f2
Zhong Jianxin <azuwis@gmail.com>
2025-07-15 19:48:08 +0800

    nix: Only bind mount /mnt if it has free space

--- nix/prepare.sh
+++ nix/prepare.sh
@@ -31,23 +31,24 @@ Linux)
     docker image prune --all --force >/dev/null
     echo
     echo "After:"
-    df -h -x tmpfs
-    echo
   fi
+  df -h -x tmpfs
+  echo
+  root_free=$(df --block-size=1 --output=avail / | sed -n 2p)
+  mnt_free=$(df --block-size=1 --output=avail /mnt | sed -n 2p)
   if [ "$BTRFS" = true ]; then
     echo "Make /nix BTRFS RAID0 from /btrfs and /mnt/btrfs"
     sudo touch /btrfs /mnt/btrfs
     sudo chmod 600 /btrfs /mnt/btrfs
-    sudo fallocate --zero-range --length "$(($(df --block-size=1 --output=avail / | sed -n 2p) - 2147483648))" /btrfs
-    sudo fallocate --zero-range --length "$(df --block-size=1 --output=avail /mnt | sed -n 2p)" /mnt/btrfs
+    sudo fallocate --zero-range --length "$((root_free - 2147483648))" /btrfs
+    sudo fallocate --zero-range --length "$mnt_free" /mnt/btrfs
     sudo losetup /dev/loop6 /btrfs
     sudo losetup /dev/loop7 /mnt/btrfs
     sudo mkfs.btrfs --data raid0 /dev/loop6 /dev/loop7
     sudo mkdir /nix
     sudo mount -t btrfs -o compress=zstd /dev/loop6 /nix
     sudo chown "${USER}:" /nix
-  elif [ "$(findmnt -bno size /mnt)" -gt 20000000000 ]; then
-    df -h -x tmpfs
+  elif [ "$mnt_free" -gt 20000000000 ]; then
     echo "/mnt is large, bind mount /mnt/nix"
     sudo install -d -o "$USER" /mnt/nix /nix
     sudo mount --bind /mnt/nix /nix
